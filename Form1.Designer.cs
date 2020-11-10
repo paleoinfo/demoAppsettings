8a3cfbf7b34e6e6382d99b3bf98403851fdd9317
paleoinfo <62340038+paleoinfo@users.noreply.github.com>
2020-11-10 18:48:13 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace demoAppsettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DimGray;
+            this.button1.Location = new System.Drawing.Point(25, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GET image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 295);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SteelBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LightCyan;
+            this.button2.Location = new System.Drawing.Point(25, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(372, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SAVE image";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "getImageNow!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using RandomPicLib;
+
+namespace demoAppsettings
+{
+    public partial class Form1 : Form
+    {
+        private AppsettingsInfo appSettingsData;
+        private string PathFolderImage;
+        public struct AppsettingsInfo
+        {
+            public string ImgFolderPath;
+            public string BackGroundColor;
+            public string LastDateTime;
+            public List<string> Topics;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //===============================================================
+            var JsonString = string.Empty;
+          
+            try
+            {
+                JsonString = System.IO.File.ReadAllText("appsettings.json");
+                appSettingsData = JsonConvert.DeserializeObject<AppsettingsInfo>(JsonString);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"errore nel file di configurazione appsettings.json");
+                System.Environment.Exit(-1);
+            }
+
+            var ImgFolderPathFromAppsettings = appSettingsData.ImgFolderPath;
+            PathFolderImage = System.IO.Path.Combine(Application.StartupPath,ImgFolderPathFromAppsettings);
+
+            try
+            {
+                if (!System.IO.Directory.Exists(PathFolderImage))
+                    System.IO.Directory.CreateDirectory(PathFolderImage);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"errore nel file di configurazione\nImgFolderPath: {ImgFolderPathFromAppsettings}");
+                System.Environment.Exit(-1);
+            }
+
+
+            //===============================================================
+
+            this.BackColor = Color.FromName(appSettingsData.BackGroundColor);
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var topics =  appSettingsData.Topics?.Count()==0?new List<string>() { "Butterfly" }: appSettingsData.Topics;
+
+            Image rndImg = await FlickrPics.getRandomPic(topics);
+
+            pictureBox1.Image = rndImg;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+
+            var fileName = Path.Combine(PathFolderImage, $"IMG_{DateTime.Now.ToString("yyyyMMddHHmmss")}.png");
+
+            try
+            {
+                pictureBox1.Image.Save(fileName);
+                MessageBox.Show($"immagine salvata in:\n{fileName}","salva", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Errore salvataggio immagine :\n{fileName}", "salva", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+          
+
+        }
+
+    }
+}
